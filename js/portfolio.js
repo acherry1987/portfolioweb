daf1e520e5789e42d62c42c789e337924fc81a16
acherry1987 <andrewcherry.v@gmail.com>
2016-04-25 00:06:50 -0400

    adding images, new javascript, updating index
    adding images, new javascript, updating index

--- js/portfolio.js
+++ js/portfolio.js
@@ -2,16 +2,19 @@
 window.onload = function () {
 
 	//About
+	var wholesite = document.getElementById("sitewrapper");
 	var contact = document.getElementById("contact");
 	var contactpage = document.getElementById("menu");
 	var close = document.getElementById("close")
 	// about_section.style.opacity = '0';
 	contact.addEventListener('click', function () {
-		contactpage.style.display = "block";
+		contactpage.style.display = "flex";
+		wholesite.style.display = "none";
 	});
 	
 	close.addEventListener('click', function () {
 		contactpage.style.display = "none";
+		wholesite.style.display = "block";
 	});
 
 }
